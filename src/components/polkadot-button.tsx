6988791119ffd183ba0c06f2e80af3d524352427
Kenji <96749852+kenjicncr@users.noreply.github.com>
2024-02-25 18:10:57 -0500

    bugfixing: new provider every time button clicked

--- src/components/polkadot-button.tsx
+++ src/components/polkadot-button.tsx
@@ -1,4 +1,5 @@
 import { WalletConnectModal } from "@walletconnect/modal";
+import UniversalProvider from "@walletconnect/universal-provider";
 import { CustomButton } from "./custom-button";
 import { useUniversalConnect } from "./universal-connect-provider";
 import { useEffect, useState } from "react";
@@ -26,13 +27,23 @@ interface PolkadotButtonProps {
   onConnectAccount: (address: string) => void;
 }
 export const PolkadotButton = ({ onConnectAccount }: PolkadotButtonProps) => {
-  const { provider } = useUniversalConnect();
+  // const { provider } = useUniversalConnect();
   const [polkadotAddress, setPolkadotAddress] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [walletConnectModal, setWalletConnectModal] =
     useState<WalletConnectModal | null>(null);
 
   const openWalletConnectModal = async () => {
+    const initializeProvider = async () => {
+      const initializedProvider = await UniversalProvider.init({
+        projectId: projectId,
+        relayUrl: "wss://relay.walletconnect.com",
+      });
+
+      return initializedProvider;
+    };
+    const provider = await initializeProvider();
+
     if (provider) {
       setIsLoading(true);
       const { uri, approval } = await provider.client.connect(params);
@@ -43,33 +54,37 @@ export const PolkadotButton = ({ onConnectAccount }: PolkadotButtonProps) => {
       setWalletConnectModal(walletConnectModal);
 
       if (uri) {
-        walletConnectModal.openModal({ uri });
+        try {
+          walletConnectModal.openModal({ uri });
 
-        console.log("waiting wallet connect approval...");
+          console.log("waiting wallet connect approval...", uri);
 
-        const walletConnectSession = await approval();
+          const walletConnectSession = await approval();
 
-        console.log("wallet connect approved");
+          console.log("wallet connect approved");
 
-        const walletConnectAccount = Object.values(
-          walletConnectSession.namespaces
-        )
-          .map((namespace) => namespace.accounts)
-          .flat();
+          const walletConnectAccount = Object.values(
+            walletConnectSession.namespaces
+          )
+            .map((namespace) => namespace.accounts)
+            .flat();
 
-        // grab account addresses from CAIP account formatted accounts
-        const accounts = walletConnectAccount.map((wcAccount) => {
-          const address = wcAccount.split(":")[2];
-          return address;
-        });
+          // grab account addresses from CAIP account formatted accounts
+          const accounts = walletConnectAccount.map((wcAccount) => {
+            const address = wcAccount.split(":")[2];
+            return address;
+          });
 
-        console.log({ accounts });
+          console.log({ accounts });
 
-        setPolkadotAddress(accounts[0]);
-        onConnectAccount && onConnectAccount(accounts[0]);
-        setIsLoading(false);
+          setPolkadotAddress(accounts[0]);
+          onConnectAccount && onConnectAccount(accounts[0]);
+          setIsLoading(false);
 
-        walletConnectModal.closeModal();
+          walletConnectModal.closeModal();
+        } catch (error) {
+          console.log(error);
+        }
       }
     }
   };
