6a8f2cd1f0cb5abf4e2105a7d96fb41aaa3bfe16
miamisan <93826913+miamisan@users.noreply.github.com>
2024-02-28 00:33:55 -0800

    fix: style upcoming events

--- src/components/landing-page-upcoming-events.tsx
+++ src/components/landing-page-upcoming-events.tsx
@@ -1,43 +1,56 @@
+"use client";
 import Link from "next/link";
-import { CustomButton } from "./custom-button";
+import { Variants, motion } from "framer-motion";
+import { Button } from "@nextui-org/react";
+import NextLink from "next/link";
+
+const scrollInLeft: Variants = {
+  offscreen: {
+    opacity: 0,
+    x: -30,
+  },
+  onscreen: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      type: "spring",
+      bounce: 0.2,
+      duration: 1,
+    },
+  },
+};
 
 export const LandingPageUpcomingEvents = () => {
   return (
-    <div className="max-w-screen-xl mx-auto">
-      <h3 className="mb-8 lg:mb-4 text-2xl font-display">Upcoming Events</h3>
-      <div
-        style={
-          {
-            // borderImageSource: `linear-gradient(to left, transparent, rgba( 255,255,255,0.3 ), transparent)`,
-          }
-        }
-        className="border-white flex flex-col lg:flex-row justify-between border border-solid items-center border-image-slice border-1 border-l-0 border-b-0 border-r-0 border-image-source from-transparent to-rgba(255, 255, 255, 0.3) py-8 px-4 rounded-5 bg-transparent transition duration-200 ease-in-out"
+    <div className="px-6">
+      <motion.div
+        variants={scrollInLeft}
+        initial="offscreen"
+        whileInView="onscreen"
+        viewport={{ once: true, amount: 0.8 }}
+        className="text-4xl w-full md:text-8xl text-white font-black py-10"
       >
-        <p>02 March</p>
-        <p>{`{ ETH Denver Official Closing Party }`}</p>
-        <Link
+        Upcoming Events
+      </motion.div>
+      <div className="border-white flex flex-col lg:flex-row justify-between border border-solid items-center border-image-slice border-1 border-l-0 border-b-0 border-r-0 border-image-source from-transparent to-rgba(255, 255, 255, 0.3) py-8 px-4 rounded-5 bg-transparent transition duration-200 ease-in-out gap-4">
+        <p className="text-primary-500">02 March</p>
+        <p>{`ETH Denver Official Closing Party`}</p>
+        <Button
+          radius="sm"
+          variant="flat"
+          className="opacity-100 hover:text-primary-300 hover:text-opacity-100"
+          as={NextLink}
           href="/register"
-          className="mt-8 lg:mt-0 w-full max-w-48 inline-flex h-8 px-7 justify-center items-center gap-10 flex-shrink-0 rounded-md border-2 border-teal-400 bg-gray-300 bg-opacity-25"
         >
-          register
-        </Link>
+          Register
+        </Button>
       </div>
-      <div
-        style={
-          {
-            // borderImageSource: `linear-gradient(to left, transparent, rgba( 255,255,255,0.3 ), transparent)`,
-          }
-        }
-        className="border-white flex flex-col lg:flex-row justify-between border border-solid items-center border-image-slice border-1 border-l-0 border-r-0 border-image-source from-transparent to-rgba(255, 255, 255, 0.3) py-8 px-4 rounded-5 bg-transparent transition duration-200 ease-in-out"
-      >
-        <p>11 July</p>
-        <p>{`{ ETH CC Brussels }`}</p>
-        <button
-          disabled
-          className="mt-8 lg:mt-0 w-full max-w-48 inline-flex h-8 px-7 justify-center items-center gap-10 flex-shrink-0 rounded-md border-2 bg-opacity-25 opacity-25"
-        >
-          register
-        </button>
+      <div className="border-white flex flex-col lg:flex-row justify-between border border-solid items-center border-image-slice border-1 border-l-0 border-r-0 border-image-source from-transparent to-rgba(255, 255, 255, 0.3) py-8 px-4 rounded-5 bg-transparent transition duration-200 ease-in-out gap-4">
+        <p className="text-primary-500">11 July</p>
+        <p>{`ETH CC Brussels`}</p>
+        <Button radius="sm" variant="flat" isDisabled>
+          Register
+        </Button>
       </div>
     </div>
   );
